5ac54d03dc091d47bba7262cc8385c4a9447faef
Pedro Brandão <phbrandao@outlook.com>
2023-10-09 22:39:17 -0300

    Ajustes na geração de palavras para não permitir palavras repitidas

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,35 @@
 
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
   const jsonData = require("../json/words.json");
   const [word, setWord] = useState("");
-  const [words, setWords] = useState<any[]>([]);
+  const [words, setWords] = useState<String[]>([]);
+  const [empty, setEmpty] = useState(false);
 
-  let wordsJSON = JSON.parse(JSON.stringify(jsonData));
+
+  useEffect(() => {
+    let wordsJSON = JSON.parse(JSON.stringify(jsonData));
+    setWords(wordsJSON);
+  }, []);
 
   const getWord = () => {
-    setWords(wordsJSON);
-    
-    let random = Math.floor(Math.random() * words.length);
-    setWord(words[random].toUpperCase());
-    console.log(words);
-    words.splice(words[random], 1);
-    console.log(words);
+    console.log(words.length);
+    if (words.length == 0) {
+      setEmpty(true);
+      setWord("as palavras acabaram");
+    } else {
+      let random = Math.floor(Math.random() * words.length);
+      setWord(words[random].toUpperCase());
+      words.splice(random, 1);
+    }
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-300">
+    <main className="flex container min-h-screen flex-col items-center justify-between p-24 bg-gray-100">
       <button
         onClick={getWord}
         type="button"
@@ -31,10 +38,17 @@ export default function Home() {
         NOVA PALAVRA
       </button>
 
-      <div className="flex min-h-screen flex-col items-center justify-between p-8 mt-8 text-gray-900 text-8xl">
-        <h1>
-          {word}
-        </h1>
+      <div className="flex min-h-screen flex-col items-center justify-between p-8 mt-8 text-gray-900">
+        {empty ? (
+          <h1 className="lg:text-5xl text-4xl text-red-500 bg-red-100 border border-red-500 rounded-lg p-4">
+            {word}
+            <p className="lg:text-xl text-xl">atualize a página para recomeçar</p>
+          </h1>
+        ) : (
+          <h1 className="lg:text-9xl text-5xl">
+            {word}
+          </h1>
+        )}
       </div>
     </main>
   )

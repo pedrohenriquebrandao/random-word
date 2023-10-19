c3558bb11320b9d858288aa03984a00b2f72a8eb
Pedro Brandão <phbrandao@outlook.com>
2023-10-19 19:24:11 -0300

    Adicionando navegação

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,29 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className="overflow-hidden">{children}</body>
+    <html lang="pt-BR">
+      <body>
+        <nav className="p-4 bg-gray-900">
+          <ol className="flex h-8 space-x-8 text-gray-100">
+          <li className="flex items-center space-x-1">
+              <Link href="/">
+                <p className="flex items-center hover:text-red-400 text-xl">Início</p>
+              </Link>
+            </li>
+            <li className="flex items-center space-x-1">
+              <Link href="/words">
+                <p className="flex items-center hover:text-red-400 text-xl">Gerador de Palavras</p>
+              </Link>
+            </li>
+            <li className="flex items-center space-x-1">
+              <Link href="/timer">
+                <p className="flex items-center hover:text-red-400 text-xl">Cronômetro</p>
+              </Link>
+            </li>
+          </ol>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
